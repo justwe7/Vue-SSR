fa15de041cd888d3c9b521dc958e1b072fc38ace
lihx <ilihuaxi@gmail.com>
2022-03-10 17:06:12 +0800

    feat: 区分生产环境开发环境(1.提取生产

--- server/index.js
+++ server/index.js
@@ -7,32 +7,32 @@ const LRU = require('lru-cache')
 //   template: require('fs').readFileSync('./index.template.html', 'utf-8')
 // })
 const { createBundleRenderer } = require('vue-server-renderer')
-const bundle = require('../dist/vue-ssr-server-bundle.json') // 用于服务端渲染的渲染数据
 const clientManifest = require('../dist/vue-ssr-client-manifest.json') // 用于客户端的渲染数据
 const resolve = file => path.resolve(__dirname, file)
 const isProd = process.env.NODE_ENV === 'production'
 // const createApp = require('./src/app')
 
-// function createRenderer (bundle, options) {
-//   // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
-//   return createBundleRenderer(bundle, Object.assign(options, {
-//     // for component caching
-//     cache: LRU({
-//       max: 1000,
-//       maxAge: 1000 * 60 * 15
-//     }),
-//     // this is only needed when vue-server-renderer is npm-linked
-//     basedir: resolve('./dist'),
-//     // recommended for performance
-//     runInNewContext: false
-//   }))
-// }
+function createRenderer (bundle, options) {
+  // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
+  return createBundleRenderer(bundle, Object.assign(options, {
+    // for component caching
+    cache: LRU({
+      max: 1000,
+      maxAge: 1000 * 60 * 15
+    }),
+    // this is only needed when vue-server-renderer is npm-linked
+    basedir: resolve('./dist'),
+    // recommended for performance
+    runInNewContext: false
+  }))
+}
 
 let renderer
 let readyPromise
 const templatePath = resolve('./src/index.template.html')
 // 生产环境使用服务端构建的包进行渲染
 if (isProd) {
+  const bundle = require('../dist/vue-ssr-server-bundle.json') // 用于服务端渲染的渲染数据
   renderer = createBundleRenderer(bundle, {
     runInNewContext: false,
     template: fs.readFileSync('./public/index.template.html', 'utf-8'),
@@ -42,13 +42,13 @@ if (isProd) {
   // 构建开发环境，本地起express服务渲染
   // In development: setup the dev server with watch and hot-reload,
   // and create a new renderer on bundle / index template update.
-  // readyPromise = require('./build/setup-dev-server')(
-  //   app,
-  //   templatePath,
-  //   (bundle, options) => {
-  //     renderer = createRenderer(bundle, options)
-  //   }
-  // )
+  readyPromise = require('./build/setup-dev-server')(
+    app,
+    templatePath,
+    (bundle, options) => {
+      renderer = createRenderer(bundle, options)
+    }
+  )
 }
 
 
@@ -75,7 +75,7 @@ server.use('/css', express.static(resolve('../dist/css')))
 server.use('/js', express.static(resolve('../dist/js')))
 
 
-function renderHtml(req, res) {
+async function renderHtml(req, res) {
   const context = {
     url: req.url,
     title: '上下文title',
